b6c041ba93f6e23e60c3343a7bc6a0728c9ae367
Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>
2022-11-15 22:34:37 +0700

    add: repository

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
 func main() {
-	print("Hi, GDSC :)")
+	print("Hi, GDSC1 :)")
+
+	s := &http.Server{
+		Addr:           ":8080",
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		responseValue := map[string]any{"Message": "Hi, GDSC3 :)"}
+		response, _ := json.Marshal(responseValue)
+		w.Write(response)
+	})
+
+	err := s.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+
 }
---
+++ backend/repository/user_db.go
@@ -0,0 +1,32 @@
+package repository
+
+// User Adapter
+import (
+	"database/sql"
+	"fmt"
+)
+
+type userRepositoryDB struct {
+	db *sql.DB
+}
+
+func NewRepositoryDB(db *sql.DB) userRepositoryDB { // รับ instance database ที่เราจะใช้มาแล้วออกมาเป็น "Adapter" ของ repository
+	return userRepositoryDB{db: db}
+}
+
+func (u userRepositoryDB) CreateUser(email string, password string, secret string) (*User, error) {
+	// implement me
+	fmt.Print("User Inserted")
+	return nil, nil
+}
+func (u userRepositoryDB) CheckUser(email string) (*User, error) {
+	// implement me
+	fmt.Print("User Fetched")
+	return nil, nil
+}
+
+func (u userRepositoryDB) GetUsers() ([]*User, error) {
+	// implement me
+	fmt.Print("Users Fetched")
+	return nil, nil
+}
